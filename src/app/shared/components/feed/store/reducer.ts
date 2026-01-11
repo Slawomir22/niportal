c862ecbe6eabdbefb116d65805a996b58f3a934c
Sławomir Majewski <majk71@10g.pl>
2026-01-11 22:44:12 +0100

    some corrections made

--- src/app/shared/components/feed/store/reducer.ts
+++ src/app/shared/components/feed/store/reducer.ts
@@ -4,22 +4,33 @@ import { feedActions } from "./actions";
 import { routerNavigatedAction } from "@ngrx/router-store";
 
 const initialState: StateFeed = {
-	isBeingLoaded: false,
-	error: null,
-	data: null
-
-}
+  isBeingLoaded: false,
+  error: null,
+  data: null,
+};
 
 const feedFunctionality = createFeature({
-	name: 'feed',
-	reducer: createReducer(
-		initialState,
-		on(feedActions.get_feed, (state) => ({ ...state, isBeingLoaded: true })),
-		on(feedActions.get_feed_success, (state, action) => ({ ...state, isBeingLoaded: false, data: action.feed })),
-		on(feedActions.get_feed_success, (state) => ({ ...state, isBeingLoaded: false })),
-		on(routerNavigatedAction, () => initialState)
-	)
+  name: "feed",
+  reducer: createReducer(
+    initialState,
+    on(feedActions.get_feed, (state) => ({ ...state, isBeingLoaded: true })),
+    on(feedActions.get_feed_success, (state, action) => ({
+      ...state,
+      isBeingLoaded: false,
+      data: action.feed,
+    })),
+    on(feedActions.get_feed_failure, (state) => ({
+      ...state,
+      isBeingLoaded: false,
+    })),
+    on(routerNavigatedAction, () => initialState)
+  ),
+});
 
-})
-
-export const { name: feedFunctionalityKey, reducer: feedFunctionalityReducer, selectIsBeingLoaded, selectError, selectData: selectFeedData } = feedFunctionality
+export const {
+  name: feedFunctionalityKey,
+  reducer: feedFunctionalityReducer,
+  selectIsBeingLoaded,
+  selectError,
+  selectData: selectFeedData,
+} = feedFunctionality;
