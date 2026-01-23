601287006830b78a14ddab4379fbe147e2cf8534
Sławomir Majewski <majk71@10g.pl>
2026-01-23 12:54:24 +0100

    some corrections made in popularTags component

--- src/app/shared/components/popularTags/popular-tags.component.ts
+++ src/app/shared/components/popularTags/popular-tags.component.ts
@@ -1,35 +1,36 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { popularTagsActions } from './store/actions';
-import { combineLatest } from 'rxjs';
-import { selectIsBeingLoaded } from '../feed/store/reducer';
-import { selectData, selectError } from './store/reducers';
-import { CommonModule } from '@angular/common';
-import { LoadingComponent } from '../loading/loading.component';
-import { ErrorMessageComponent } from '../error-message/error-message.component';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { popularTagsActions } from "./store/actions";
+import { combineLatest } from "rxjs";
+import { selectIsBeingLoaded } from "../feed/store/reducer";
+import { selectData, selectError } from "./store/reducers";
+import { CommonModule } from "@angular/common";
+import { LoadingComponent } from "../loading/loading.component";
+import { ErrorMessageComponent } from "../error-message/error-message.component";
+import { RouterLink } from "@angular/router";
+import { Tag } from "../../models/tag";
 
 @Component({
-	selector: 'app-popular-tags',
-	templateUrl: './popular-tags.component.html',
-	styleUrls: ['./popular-tags.component.css'],
-	standalone: true,
-	imports: [CommonModule, LoadingComponent, ErrorMessageComponent, RouterLink]
+  selector: "app-popular-tags",
+  templateUrl: "./popular-tags.component.html",
+  styleUrls: ["./popular-tags.component.css"],
+  standalone: true,
+  imports: [CommonModule, LoadingComponent, ErrorMessageComponent, RouterLink],
 })
 export class PopularTagsComponent implements OnInit {
-	private store = inject(Store);
+  private store = inject(Store);
 
-	popularTagsData$ = combineLatest({
-		isBeingLoaded: this.store.select(selectIsBeingLoaded),
-		error: this.store.select(selectError),
-		data: this.store.select(selectData)
+  popularTagsData$ = combineLatest({
+    isBeingLoaded: this.store.select(selectIsBeingLoaded),
+    error: this.store.select(selectError),
+    data: this.store.select(selectData),
+  });
 
-	})
+  ngOnInit(): void {
+    this.store.dispatch(popularTagsActions.get_popular_tags());
+  }
 
-	ngOnInit(): void {
-		this.store.dispatch(popularTagsActions.get_popular_tags());
-	}
-
-
-
+  trackByTag(index: number, tag: Tag): Tag {
+    return tag;
+  }
 }
