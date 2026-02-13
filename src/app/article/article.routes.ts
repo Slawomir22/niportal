c021c071be47252b85d67949ae7e7d97d1869e62
Sławomir Majewski <majk71@10g.pl>
2026-02-13 20:08:59 +0100

    name corrected in article.routes,ts

--- src/app/article/article.routes.ts
+++ src/app/article/article.routes.ts
@@ -1,20 +1,22 @@
 import { Route } from "@angular/router";
 import { ArticleComponent } from "./components/article/article.component";
 import { provideEffects } from "@ngrx/effects";
-import * as allArticlEffects from "./store/effects";
+import * as allArticelEffects from "./store/effects";
 import { provideState } from "@ngrx/store";
-import { articleFunctionalityKey, articleFunctionalityReducer } from "./store/reducer";
+import {
+  articleFunctionalityKey,
+  articleFunctionalityReducer,
+} from "./store/reducer";
 import { ArticleService } from "./services/article.service";
 
 export const articleRoutes: Route[] = [
-	{
-		path: '',
-		component: ArticleComponent,
-		providers: [
-			provideEffects(allArticlEffects),
-			provideState(articleFunctionalityKey, articleFunctionalityReducer),
-			ArticleService
-		]
-	},
-
-]
+  {
+    path: "",
+    component: ArticleComponent,
+    providers: [
+      provideEffects(allArticelEffects),
+      provideState(articleFunctionalityKey, articleFunctionalityReducer),
+      ArticleService,
+    ],
+  },
+];
